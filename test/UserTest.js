52b420e6d863e9963895fb6f9a849ab12cc97fb3
Frank Murphy <fmurphy@instructure.com>
2018-07-24 22:58:06 -0400

    Working User

---
+++ test/UserTest.js
@@ -0,0 +1,37 @@
+/* eslint-env node, mocha */
+import { expect } from 'chai'
+import sqlite3 from 'sqlite3'
+import uuid from 'uuid'
+
+import User from '../lib/User'
+import { Server } from './fakes'
+import Keymaster from './fixtures/Keymaster'
+import { login } from './fixtures'
+
+describe('User', () => {
+  const db = () => new sqlite3.Database(':memory:')
+
+  it('creates new default state', async () => {
+    const server = new Server()
+    const user = new User(db(), server, await login())
+    const data = await user.getStore(Keymaster)
+    expect(await data.index()).to.deep.equal([0, 0])
+  })
+
+  it('loads existing state when already present', async () => {
+    const server = new Server()
+    const user = new User(db(), server, await login())
+    const keymaster = await user.getStore(Keymaster)
+
+    const [ parent ] = await keymaster.groups.create([
+      { name: 'All Passwords' }
+    ])
+    await keymaster.sync()
+
+    const reloaded = await new User(db(), server, await login())
+    const recreated = await reloaded.getStore(Keymaster)
+    await recreated.sync()
+    const reparent = await recreated.groups.reference(parent.id).fetch()
+    expect(reparent.name).to.equal('All Passwords')
+  })
+})
--- test/fixtures.js
+++ test/fixtures.js
@@ -1,24 +1,5 @@
-import assert from 'assert'
-import immutable, { fromJS } from 'immutable'
-
-/*
-import { makeAction, crud } from '../lib/actions'
-import { validate, assign } from '../lib/models'
-import { idFrom, nonEmptyString } from '../lib/validate'
 import { BoxKeyPair } from '../lib/crypto'
-import { State as _State } from '../lib/state'
-import { User } from '../lib/user'
-
-export const actions = makeActionCreator({ VERSION: 1 })
-const counters = crud('counters', Model)
-export const actions = combineReducers({
-    counters: counters.reducer
-})
-export counter
-
-const id = uuid.v4()
-const nextState = state.addCounter({ id , count: 1 })
-const _counter = state.counter.get(id)
+import User from '../lib/User'
 
 let _cachedLogin = null
 export const login = async () => {
@@ -33,12 +14,3 @@ export const login = async () => {
 
     return _cachedLogin
 }
-
-export class State extends _State {
-    getCounter (id) {
-        return this._cache.get(id)
-    }
-}
-State.Action = Action
-State.start = start
-*/
