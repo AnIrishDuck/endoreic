ce8d822b7613421c280578fcd7b945999ffacda8
Frank Murphy <fmurphy@instructure.com>
2019-01-10 21:52:34 -0500

    Refactor out keyring functionality

---
+++ test/KeyringTest.js
@@ -0,0 +1,40 @@
+/* eslint-env node, mocha */
+import chai, { expect } from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+
+import Keyring from '../lib/Keyring'
+import { BoxKeyPair } from '../lib/crypto'
+import { Server } from '../lib/fakes'
+
+chai.use(chaiAsPromised)
+
+describe('Keyring', () => {
+  const user = new BoxKeyPair()
+
+  it('can be created and includes creator key', async () => {
+    const server = new Server()
+    const ring = await Keyring.create(server, user)
+    const { read } = await ring.fetch(user)
+
+    const ciphertext = read.encrypt(new Buffer('too many secrets'))
+
+    const other = await (new Keyring(server, ring.id)).fetch(user)
+    expect(other.read.decrypt(ciphertext).toString())
+      .to.equal('too many secrets')
+  })
+
+  it('can include additional guest keys', async () => {
+    const server = new Server()
+    const ring = await Keyring.create(server, user)
+    const { write } = await ring.fetch(user)
+
+    const guest = new BoxKeyPair()
+    await ring.add(user, guest)
+
+    const ciphertext = guest.encrypt(write.publicKey(), new Buffer('too many secrets'))
+
+    const other = await (new Keyring(server, ring.id)).fetch(guest)
+    expect(other.write.decrypt(guest.publicKey(), ciphertext).toString())
+      .to.equal('too many secrets')
+  })
+})
